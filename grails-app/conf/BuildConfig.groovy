d63557dc4d8c3e4f9bcf9c86fc3919f7cf5de724
Craig Burke <craig@craigburke.com>
2014-05-23 10:17:15 -0400

    Bumping to grails 2.4.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,12 +1,21 @@
 grails.project.work.dir = 'target'
 
+grails.project.fork = [
+        test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+        run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+        war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+        console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+]
+
+grails.project.dependency.resolver = "maven"
+
 grails.project.dependency.resolution = {
 
     inherits 'global'
     log 'warn'
     legacyResolve false
 
-    String gebVersion = '0.9.0'
+    String gebVersion = '0.9.2'
     String seleniumVersion = '2.39.0'
     String spockVersion = '0.7'
 
@@ -17,13 +26,10 @@ grails.project.dependency.resolution = {
     }
 
     dependencies {
-        test("org.seleniumhq.selenium:selenium-htmlunit-driver:$seleniumVersion") {
-            excludes 'xml-apis'
-        }
-
+        test "org.seleniumhq.selenium:selenium-htmlunit-driver:$seleniumVersion"
         test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
         test "org.seleniumhq.selenium:selenium-support:$seleniumVersion"
-        test "org.gebish:geb-spock:$gebVersion"
+        test "org.gebish:geb-spock:${gebVersion}"
 
         test "org.seleniumhq.selenium:selenium-remote-driver:$seleniumVersion"
         test "org.seleniumhq.selenium:selenium-firefox-driver:$seleniumVersion"
@@ -33,9 +39,9 @@ grails.project.dependency.resolution = {
     plugins {
         runtime ":asset-pipeline:1.8.4"
 
-        build(":tomcat:$grailsVersion",
-              ":release:2.2.1",
-              ":rest-client-builder:1.0.3") {
+        build(":release:3.0.1",
+              ":rest-client-builder:1.0.3",
+              ":tomcat:7.0.53") {
             export = false
         }
 
