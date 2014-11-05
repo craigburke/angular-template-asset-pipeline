95d8f77389c3ee42f84dd1b670f6d3b8638bd817
Craig Burke <craig@craigburke.com>
2014-11-05 12:36:52 -0500

    Initial commit

---
+++ src/main/groovy/com/craigburke/angular/HTMLTemplateProcessor.groovy
@@ -0,0 +1,29 @@
+package com.craigburke.angular
+
+import asset.pipeline.AssetCompiler
+import asset.pipeline.AssetPipelineConfigHolder
+import static com.craigburke.angular.TemplateProcessorUtil.*
+
+class HTMLTemplateProcessor {
+
+    HTMLTemplateProcessor(AssetCompiler precompiler) { }
+
+    def process(input, assetFile) {
+		def config = AssetPipelineConfigHolder.config.angular
+		
+        File file = assetFile.file		
+		String templateFolder = config?.templateFolder ?: "templates"
+		
+        String moduleName = getModuleName(file, templateFolder)
+
+		boolean includePathInName = config?.hasProperty('includePathInName') ? config.includePathInName : false
+        String templateName = getTemplateName(file, templateFolder, includePathInName)
+		
+	    boolean compressHtml = config?.hasProperty('compressHtml') ? config.compressHtml : true
+		boolean preserveHtmlComments = config?.hasProperty('preserveHtmlComments') ? config.preserveHtmlComments : false
+		String content = formatHtml(input.toString(), compressHtml, preserveHtmlComments)
+
+        getTemplateJs(moduleName, templateName, content)
+    }
+
+}
---
+++ src/main/resources/META-INF/assets.spec
@@ -0,0 +1,1 @@
+com.craigburke.angular.HTMLTemplateAssetFile
