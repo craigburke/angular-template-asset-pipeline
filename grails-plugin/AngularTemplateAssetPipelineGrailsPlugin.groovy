2c6ec4606e15f131426c59d8d95b91d627b98735
Craig Burke <craig@craigburke.com>
2015-10-12 14:22:08 -0400

    Bumping version to 2.2.5

--- grails-plugin/AngularTemplateAssetPipelineGrailsPlugin.groovy
+++ grails-plugin/AngularTemplateAssetPipelineGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class AngularTemplateAssetPipelineGrailsPlugin {
 	
-    def version = "2.2.4"
+    def version = "2.2.5"
     def grailsVersion = "2.0 > *"
 
     def title = "AngularJS Template Asset-Pipeline Plugin"
